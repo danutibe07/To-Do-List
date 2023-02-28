4f63c920a8dc49132cdfce4f12ea973d1d990013
danutibe07 <danielutibe07@gmail.com>
2023-02-28 12:08:36 +0100

    updates to index.js and index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import _ from 'lodash';
-import './style.css';
+ import _ from "lodash";
+ import "./style.css";
+const tasks = [
+    {
+      description: 'Read on Algorithms',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'Complete section 2 of Rails bootcamp',
+      completed: false,
+      index: 1,
+    },
+  ];
+  
+  const pendingTasks = (tasks) => {
+    tasks.sort((a, b) => a.index - b.index);
+    const todoContent = document.getElementById('content');
+    todoContent.innerHTML = '';
+    tasks.forEach((task) => {
+      const template = `  <li class="task">
+           <input type="checkbox" class="checkme">
+           <input type="text" value="${todo.description}" class="tasks" id="item${todo.index}" readonly>
+           <i class="fa-solid uil-ellipsis-v edit" id="edit${todo.index}"></i>
+           <i class="fa-solid fa-trash delete" id="delete${todo.index}"></i>
+           </li>`;
+      todoContent.innerHTML += template;
+    });
+  };
 
-const finished = false;
-let index = 0;
-let container = [];
+  pendingTasks(tasks);
