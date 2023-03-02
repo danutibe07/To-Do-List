ff50defda8562e3562234ec05b5ab6d25a4d9838
danutibe07 <danielutibe07@gmail.com>
2023-03-02 03:58:45 +0100

    updates

--- src/modules/Task.js
+++
@@ -1,8 +0,0 @@
-export default class Task {
-    constructor(description, index, completed = false) {
-      this.description = description;
-      this.index = index;
-      this.completed = completed;
-    }
-  }
-  
