9e9b7ef5585b9795727222a1e587f46510995a43
danutibe07 <danielutibe07@gmail.com>
2023-03-02 03:53:00 +0100

    updates to index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,101 @@
 import './style.css';
+import './modules/todotask.js';
 
-const tasks = [
-  {
-    description: 'Read on Algorithms',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Complete Rails bootcamp',
-    completed: true,
-    index: 1,
-  },
-];
+const toDoList = document.querySelector('.content-container');
 
-const todoList = document.querySelector('.content');
-
-const pendingTasks = () => {
+// load saved tasks
+const loadTask = (tasks) => {
+  tasks.sort((a, b) => a.index - b.index);
+  toDoList.innerHTML = '';
   tasks.forEach((task) => {
-    // Creating a list element and assigning it a className
-    const list = document.createElement('li');
-    // list.innerText = task.description;
-    // Creating an i element for edit, assigning it a className and giving it an id
-    const edit = document.createElement('i');
-    edit.className = ' fas fa-solid fas fas fa-ellipsis-v edit-icon';
-    edit.setAttribute('ide', task.index);
-    // Creating an i element for deleting a task and assigning it a className
-    const destroy = document.createElement('i');
-    destroy.className = 'remove-icon icon-hidden fas fa-solid fa-trash';
-    destroy.setAttribute('ide', task.index);
-
-    list.setAttribute('ide', task.index);
-    list.innerHTML = `<div class="todo-item-content">
-       <input class="checkbox" ide=${task.index} type="checkbox" />
-       <textarea class="todo-description" ide=${task.index} rows="1" maxlength="100">${task.description}</textarea> 
-      </div>`;
-    list.appendChild(edit);
-    // list.appendChild(destroy);
-    todoList.appendChild(list);
+    const template = ` <li>
+                          <input type="checkbox" class="checkbox" data-index="${task.index}" ${task.completed ? 'checked' : ''} class="me-2 mt-2" />
+                          <input type="text" class="task-description" data-index="${task.index}" value ="${task.description}" />
+                          <div class="buttons">
+                            <div class="ellipsis">
+                              <button class="btn-ellipsis" data-index="${task.index}">
+                                <i class="fas fa-solid fas fas fa-ellipsis-v"></i>
+                              </button>
+                            </div>
+                            <div class="hidden">
+                              <button class="btn-trash" data-index="${task.index}">
+                                <i class="fas fa-solid fa-trash"></i>
+                              </button>
+                            </div>
+                          </div>
+                        </li>`;
+    toDoList.innerHTML += template;
   });
 };
-pendingTasks(tasks);
+
+// add new task
+const addTask = () => {
+  const add = document.querySelector('.task-input').value;
+  const tasks = JSON.parse(localStorage.getItem('tasks') || JSON.stringify([]));
+  const newTask = {
+    description: add,
+    completed: false,
+    index: tasks.length + 1,
+  };
+
+  tasks.push(newTask);
+  loadTask(tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+// remove a task
+const removeTask = (value) => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  const filtered = tasks.filter((task) => task.index !== parseInt(value, 10));
+  filtered.forEach((task, i) => {
+    task.index = i + 1;
+  });
+  localStorage.setItem('tasks', JSON.stringify(filtered));
+  loadTask(filtered);
+};
+
+// calling addTask() when user enters
+const input = document.querySelector('.task-input');
+input.addEventListener('keyup', (event) => {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
+
+// loading Tasks when page loads
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+if (tasks) loadTask(tasks);
+
+// removing tasks
+toDoList.addEventListener('click', (e) => {
+  if (e.target.closest('.btn-ellipsis')) {
+    const ellipsisBtn = e.target.closest('.btn-ellipsis');
+    const trashBtn = ellipsisBtn.closest('.ellipsis').nextElementSibling;
+    ellipsisBtn.classList.add('hidden');
+    trashBtn.classList.remove('hidden');
+  } else if (e.target.closest('.btn-trash')) {
+    const trashBtn = e.target.closest('.btn-trash');
+    removeTask(trashBtn.dataset.index);
+  }
+});
+
+// check the boxes
+toDoList.addEventListener('change', (event) => {
+  if (event.target.type === 'checkbox') {
+    const index = parseInt(event.target.dataset.index, 10);
+    const task = tasks.find((task) => task.index === index);
+    task.completed = event.target.checked;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+});
+
+// editing the todos
+toDoList.addEventListener('keyup', (event) => {
+  if (event.target.classList.contains('task-description')) {
+    const index = parseInt(event.target.closest('input').dataset.index, 10);
+    const task = tasks.find((task) => task.index === index);
+    task.description = event.target.value;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+});
