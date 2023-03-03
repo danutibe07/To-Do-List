eb458ddaa06ed216696c002f47b85270a65e5417
danutibe07 <danielutibe07@gmail.com>
2023-03-03 18:06:12 +0100

    updates for todo app

--- src/index.js
+++ src/index.js
@@ -96,13 +96,5 @@ toDoList.addEventListener('keyup', (event) => {
   }
 });
 
-// clear all completed tasks
-const clearBtn = document.getElementById('clearall');
-clearBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const tasks = JSON.parse(localStorage.getItem('tasks'));
-  const filtered = tasks.filter((task) => !task.completed);
-  filtered.forEach((task, i) => { task.index = i + 1; });
-  localStorage.setItem('tasks', JSON.stringify(filtered));
-  loadTask(filtered);
-});
+
+AudioDest
